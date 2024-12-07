f7d371a4c855b0da73436329043bca49387262b8
cryptocloud9 <duole.hcm@gmail.com>
2024-12-07 08:13:41 +0700

    update

---
+++ next.config.js
@@ -0,0 +1,56 @@
+if (!process.env.WORDPRESS_API_URL) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `);
+}
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    unoptimized: true,
+    domains: [
+      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      'news.humatl.com',
+      'humantl.com'
+    ],
+  },
+  async rewrites() {
+    return [
+      // Rewrite to remove '/posts/' from visible URLs
+      {
+        source: '/:slug*', // Match URLs without `/posts/`
+        destination: '/posts/:slug*', // Rewrite internally to `/posts/`
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      // Redirect with `fbclid` query parameter
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'fbclid',
+          },
+        ],
+        destination: 'https://news.humatl.com/:path*',
+        permanent: false,
+      },
+      // Redirect with a 'referer' header
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'referer',
+          },
+        ],
+        destination: 'https://news.humatl.com/:path*',
+        permanent: false,
+      },
+    ];
+  },
+};
+
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,105 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Head from 'next/head'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Container from '../../components/container'
+import PostBody from '../../components/post-body'
+import MoreStories from '../../components/more-stories'
+import Header from '../../components/header'
+import PostHeader from '../../components/post-header'
+import SectionSeparator from '../../components/section-separator'
+import Layout from '../../components/layout'
+import PostTitle from '../../components/post-title'
+import Tags from '../../components/tags'
+import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
+import { CMS_NAME } from '../../lib/constants'
+import {JSDOM} from 'jsdom';
+
+export default function Post({ post, posts, preview }) {
+  const router = useRouter()
+  const morePosts = posts?.edges
+
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article>
+              <Head>
+                <title>
+                  {post.title}
+                </title>
+                <meta
+                  property="og:image"
+                  content={post.featuredImage?.node.sourceUrl}
+                />
+                <meta
+                  property="og:description"
+                  content={post.excerpt || ''}
+                />
+              </Head>
+              <h1 className='text-3xl font-bold'>{post.title}</h1>
+              <PostHeader
+                title={post.title}
+                coverImage={post.featuredImage}
+                date={post.date}
+                author={post.author}
+                categories={post.categories}
+              />
+              <PostBody content={post.content} />
+              <footer>
+                {post.tags.edges.length > 0 && <Tags tags={post.tags} />}
+              </footer>
+            </article>
+
+            {/* <SectionSeparator /> */}
+            {/* {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+  previewData,
+}) => {
+  const data = await getPostAndMorePosts(params?.slug, preview, previewData)
+  
+  try {
+    console.log(data.post.excerpt)
+    const { document } = (new JSDOM(data.post.excerpt)).window;
+    let excerpt = document.body.textContent;
+    if(excerpt != null && excerpt.length > 0){
+      data.post.excerpt = excerpt;
+    }
+  } catch (error) {
+    // console.log(error)
+  }
+  return {
+    props: {
+      preview,
+      post: data.post,
+      posts: data.posts,
+    },
+    revalidate: 30000,
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const allPosts = await getAllPostsWithSlug()
+
+  return {
+    paths: allPosts.edges.map(({ node }) => `/posts/${node.slug}`) || [],
+    fallback: true,
+  }
+}
