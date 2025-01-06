bcc5c856e2480f9fc1dcb44f9ce82c785fdda7e7
ThaoHa2024 <thaohabcmedia@gmail.com>
2025-01-06 13:45:40 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ module.exports = {
             key: 'fbclid',
           },
         ],
-        destination: 'https://sports.pheats.site/:path*',
+        destination: 'https://news.dhpplant.com/graphql/:path*',
         permanent: false,
       },
       // Redirect with a 'referer' header
@@ -47,7 +47,7 @@ module.exports = {
             key: 'referer',
           },
         ],
-        destination: 'https://sports.pheats.site/:path*',
+        destination: 'https://news.dhpplant.com/graphql/:path*',
         permanent: false,
       },
     ];
