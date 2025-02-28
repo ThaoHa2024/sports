295e724aea0d644919f6be6019252c51c18dee63
ThaoHa2024 <thaohabcmedia@gmail.com>
2025-02-28 13:45:17 +0700

    Update next.config.js news.humatl.com

--- next.config.js
+++ next.config.js
@@ -11,8 +11,7 @@ module.exports = {
     unoptimized: true,
     domains: [
       process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
-      'sports.pheats.site',
-      'pheats.site'
+      'news.humatl.com'
     ],
   },
   async rewrites() {
